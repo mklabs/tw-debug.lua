771a00c557fc2ad57a0b7a2cc24264ed5273853b
Mickael Daniel <daniel.mickael@gmail.com>
2020-10-02 20:48:58 +0200

    Fix formatters count when a non string is passed as first argument

--- script/_lib/tw-debug.lua
+++ script/_lib/tw-debug.lua
@@ -1,3 +1,27 @@
+--[[
+    MIT License
+
+    Copyright (c) 2020 Mickael Daniel
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+--]]
+
 local inspect = require("vendors/inspect")
 local inspectOptions = { depth = 4 }
 
@@ -83,8 +107,12 @@ end
 function debug.log(env, ...)
     local str = env.namespace .. " "
 
-    local formatersCount = debug.countChar(arg[1], "%%[^%s]*%a")
+    local formatersCount = 0
     local startIndex = 1
+
+    if type(arg[1]) == "string" then
+        formatersCount = debug.countChar(arg[1], "%%[^%s]*%a")
+    end
 
     if formatersCount ~= 0 then
         local params = debug.slice(arg, 2)
